e6d77724a8af707f910cf68ddccd739d0dd5b5ee
DShevrd1n <87382854+DShevrd1n@users.noreply.github.com>
2021-07-14 17:32:33 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    class Person
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+        public Person(int Age,string Name)
+        {
+            this.Age = Age;
+            this.Name = Name;
+        }
+        public Person()
+        {
+
+        }
+        public string Method1() => ($"Person's Age {Age} Person's Name {Name}");
+
+        
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFrom("C:/Users/XoMe/source/repos/ConsoleApp1/ConsoleApp1/bin/Debug/ConsoleApp1.exe");
+            Console.WriteLine(asm.FullName);
+
+            Type t = asm.GetType("ConsoleApp1.Person", true, true);
+
+            object obj = Activator.CreateInstance(t);
+
+            MethodInfo method = t.GetMethod("Method1");
+            object result = method.Invoke(obj, new object[] { });
+
+            Console.WriteLine((result));
+            Console.ReadLine();
+        }
+    }
+}
